80efcc5e3d1887e5d77c34b858cd15415ffd4223
Gemmy2010 <jemimaosoro@gmail.com>
2022-06-17 16:42:13 +0200

    counter modification

--- src/components/CounterViews.js
+++ src/components/CounterViews.js
@@ -1,27 +1,33 @@
-import React from 'react';
-
-
+import React,{useEffect, useState} from "react";
 
 function CounterViews() {
-    const countEl = document.getElementById("count");
+  const [totalCount, setTotalCount] = useState(0);
 
-     const myCount=fetch("https://api.countapi.xyz/update/biketrace/bikeblog/?amount=1")
-      .then((res) => res.json())
-      .then(data =>{
-          countEl.innerHTML = data.value
-          
-      });
+  const getCount = async () => {
+    const response = await fetch(
+      "https://api.countapi.xyz/update/biketrace/bikeblog/?amount=1"
+    );
+    const count = await response.json();
+
+    return count.value
+  };
+
+  useEffect(async () => {
+  
+    setTotalCount(await getCount())
+   
     
+  }, []);
 
   return (
     <div id="views">
       <h1 className="views_header">Number of visitors</h1>
       <div className="views_card">
         <h2 className="view_number">The total number of visitors</h2>
-        <h2 id="count">0</h2>
+        <h2 id="count">{totalCount}</h2>
       </div>
     </div>
   );
 }
 
-export default CounterViews
+export default CounterViews;
